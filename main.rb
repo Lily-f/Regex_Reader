23610eb564e92604ad176793ea7fda45ce00f77e
faheylily <faheylily@ecs.vuw.ac.nz>
2021-03-04 12:56:32 +1300

    Adds description for handling multiple alternations and groups in a regex

--- main.rb
+++ main.rb
@@ -55,16 +55,39 @@ read_regex('ab.|(cd)*')
 
 # reach characters until an element is made. Give this element a depth based on group positions (base is depth 0)
 # if this element is NOT an alternate NOR alternation then read ahead by one
-# if this extra one is an alternation character then handle like so: create element using the current read characters,
-# and store current elements from same group level or greater inside the alternation element and ONE of the options.
+
+# if this extra one is an alternation character then handle: create alt element using the current read characters,
+# and store current elements from same group level or greater inside the alternation element as ONE of the options.
+# remove these from array of elements
 # Read ahead to find what is on the other side of the alternation and repeat handling
 # Note that reading must stop if an open bracket is detected to the left or if close brackets are detected to the right
 # such that the group holding the alternation is escaped / the depth becomes lower than alternation.
+# when the alternation element is created at the start it should be stored separately from the element array
+# this is so that if the alternation contains groups,
+# the groups will be added to the alternation instead of the elements
+# Note this is only for groups within the alternation, not groups at a lower depth
+# Note that alternations can only exist within another alternation if inside a group TODO: HANDLE
+# need alternation stack like group stack. same rules apply though, just prevents loss of info
+
 # ELSE if is an asterisk then set the original element as repeatable. Note repeatable is [0..infinity] times
 # if the extra char was not either an asterisk or pipe then after handling the original element,
 # set it as original element and repeat until there are no characters
+
 # if an open bracket is read then handle groups like so:
-# create a group element that is initially empty. store group onto stack of groups. Then repeat the above method or reading chars
-# until close brackets are encountered, which remove a group from the stack.
-# If it was the last group then place it on the array of elements
+# create a group element that is initially empty. store group onto stack of groups. Then repeat the above methods
+# until close brackets are encountered, which remove a group from the stack and decrease depth.
+# If it was the last group then place it on the array of elements OR into current alternation if one is open
 # Note that groups can be repeatable so read one ahead as other elements
+
+# Handling of character reader
+# any regular character is handled the same way regardless of circumstance
+# groups are handled differently and thus need method for controlling them
+# asterisks require a non alternation element on the left. char before can't be a '|' or '('
+# groups must be closed
+
+# Add elements to the group at the top of the stack iff exists and depth greater than alternation
+# if group has same depth as alternation then add to alternation, not group
+# remove group from stack and add to alternation. If depth is now lower than alternation then
+# move alternation to elements
+# only add elements directly to elements array if there are no groups or alternations in either stack
+# TODO: Handle group and alternation behavior when combined
